3c483cff81f646163fa9bcee1aec03f9f6901882
Amanda Brown <amanda.brown@gmail.com>
2016-10-26 14:33:49 -0400

    initial commit

---
+++ CrystalBallApp/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CrystalBallApp.Models;
+
+namespace CrystalBallApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult ShakeBall()
+        {
+            CrystalBall ball = new CrystalBall();
+            string reply = ball.getResponse();
+            return Json(reply, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ CrystalBallApp/Models/CrystalBall.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrystalBallApp.Models
+{
+    public class CrystalBall
+    {
+        //get random number
+        //create array of strings for responses
+
+        public string getResponse()
+        {
+            string[] responses = { "Geronimo!", "I like bananas. Bananas are good.", "Allonsy!", "Run!", "Planet of the pudding brains!" };
+
+            Random randomNum = new Random();
+            var num = randomNum.Next(0, responses.Length);
+            return responses[num];
+        }
+    }
+}
